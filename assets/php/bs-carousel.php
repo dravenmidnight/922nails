ccade27980ebfa2f721d9ccbc450cbb427c614bc
Draven Roche <artistdash@gmail.com>
2019-07-14 16:47:17 -0700

    updates icons on home slider.

--- assets/php/bs-carousel.php
+++ assets/php/bs-carousel.php
@@ -20,11 +20,11 @@
 
 		<!-- Controls -->
 		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<i class="fas fa-chevron-left icon-prev"></i>
 			<span class="sr-only">Previous</span>
 		</a>
 		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<i class="fas fa-chevron-right icon-next"></i>
 			<span class="sr-only">Next</span>
 		</a>
 		
