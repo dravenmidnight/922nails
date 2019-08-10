5ed6bc4e601351d3bb5840533c088939f2f94a4d
Draven Roche <artistdash@gmail.com>
2019-08-09 17:25:34 -0700

    1. removes coupon URL from sitemap. 2. updates & reorganizes gallery assets.

--- gallery/gallery-content.php
+++ gallery/gallery-content.php
@@ -10,93 +10,93 @@
 }
 
 .img-1 {
-	background-image: url(../assets/img/gallery/1513654680_31a.jpg);
+	background-image: url(../assets/img/gallery/acrylic_IMG_7021.jpg);
 }
 .img-2 {
-	background-image: url(../assets/img/gallery/1513654680_24a.jpg);
+	background-image: url(../assets/img/gallery/acrylic_IMG_7023.jpg);
 }
 .img-3 {
-	background-image: url(../assets/img/gallery/1513654680_23a.jpg);
+	background-image: url(../assets/img/gallery/acrylic_IMG_7024.jpg);
 }
 .img-4 {
-	background-image: url(../assets/img/gallery/1513654680_22a.jpg);
+	background-image: url(../assets/img/gallery/acrylic_IMG_7025.jpg);
 }
 .img-5 {
-	background-image: url(../assets/img/gallery/1513654680_21a.jpg);
+	background-image: url(../assets/img/gallery/unnamed-1-810x1080.jpg);
 }
 .img-6 {
-	background-image: url(../assets/img/gallery/1513654680_17a.jpg);
+	background-image: url(../assets/img/gallery/unnamed-3-1080x810.jpg);
 }
 .img-7 {
-	background-image: url(../assets/img/gallery/1513654680_16a.jpg);
+	background-image: url(../assets/img/gallery/unnamed-4-1080x810.jpg);
 }
 .img-8 {
-	background-image: url(../assets/img/gallery/1513654680_15a.jpg);
+	background-image: url(../assets/img/gallery/unnamed-5-768x1024.jpg);
 }
 .img-9 {
-	background-image: url(../assets/img/gallery/1513654680_14a.jpg);
+	background-image: url(../assets/img/gallery/unnamed-6-768x1024.jpg);
 }
 
 </style>
 <section class="container mb-30">
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_31a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/acrylic_IMG_7021.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-1"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_24a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/acrylic_IMG_7023.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-2"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_23a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/acrylic_IMG_7024.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-3"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_22a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/acrylic_IMG_7025.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-4"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_21a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/unnamed-1-810x1080.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-5"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_17a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/unnamed-3-1080x810.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-6"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_16a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/unnamed-4-1080x810.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-7"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-6 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_15a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/unnamed-5-768x1024.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-8"></div>
 			</div>
 		</a>
 	</div>
 	<div class="col-sm-6 col-md-4 col-xs-12 p-0">
-		<a href="<?php echo $aPATH ?>img/gallery/1513654680_14a.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
+		<a href="<?php echo $aPATH ?>img/gallery/unnamed-6-768x1024.jpg" class="gallery-item gallery-item-popup thumbnail" data-group="gallery-1" title="Gallery" itemprop="url">
 			<div class="gallery-box">
 				<div class="gallery-image img-9"></div>
 			</div>
