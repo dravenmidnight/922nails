8ba37643dd616954255e6f5f0d3e023647d1e12d
Draven Roche <artistdash@gmail.com>
2019-08-09 17:31:40 -0700

    updates static HTML to reflect new navs, services & gallery content.

--- assets/php/footer-nav.php
+++ assets/php/footer-nav.php
@@ -5,11 +5,11 @@
 	<a href="/services/" class="nav--link list-group-item h5">
 		Services
 	</a>
-	<a href="/coupon/" class="nav--link list-group-item h5">
-		Coupon
-	</a>
 	<a href="/gallery/" class="nav--link list-group-item h5">
 		Gallery
+	</a>
+	<a href="/about/" class="nav--link list-group-item h5">
+		About
 	</a>
 	<a href="/contact/" class="nav--link list-group-item h5">
 		Contact
